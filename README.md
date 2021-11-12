0e2d1e135eb9cf5dd2d43dba14f0616baf061559
Álex Sáez <alexsaezm@gmail.com>
2021-11-12 10:07:36 +0100

    Add clean up old kernels script for Fedora
    Changes to be committed:
    	modified:   README.md
    	new file:   clean-up_old_kernels_fedora.sh

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # scripts
-Random scripts I use from time to time
+Random scripts I use from time to time.
+
+> Use at your own discretion
+
+Some of them are not mine or are heavily based on others out there.
+
---
+++ clean-up_old_kernels_fedora.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Not mine: https://docs.fedoraproject.org/en-US/quick-docs/dnf-system-upgrade/#sect-clean-up-old-kernels
+
+old_kernels=($(dnf repoquery --installonly --latest-limit=-1 -q))
+if [ "${#old_kernels[@]}" -eq 0 ]; then
+    echo "No old kernels found"
+    exit 0
+fi
+
+if ! sudo dnf remove "${old_kernels[@]}"; then
+    echo "Failed to remove old kernels"
+    exit 1
+fi
+
+echo "Removed old kernels"
+exit 0
